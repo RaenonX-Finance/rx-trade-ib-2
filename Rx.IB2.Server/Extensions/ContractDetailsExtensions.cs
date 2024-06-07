a60baf2d43255493c820036e31df34a5fff07109
RaenonX <raenonx0710@gmail.com>
2024-06-07 16:57:44 -0500

    Fixed failed trading date interval after Friday market close

--- Rx.IB2.Server/Extensions/ContractDetailsExtensions.cs
+++ Rx.IB2.Server/Extensions/ContractDetailsExtensions.cs
@@ -5,6 +5,11 @@ namespace Rx.IB2.Extensions;
 
 public static class ContractDetailsExtensions {
     public static DateInterval[] ToTradingDateIntervals(this ContractDetails contractDetails) {
+        // `TimeZoneId` and/or `TradingHours` could be null, at least after the Friday market close on options
+        if (contractDetails.TimeZoneId is null || contractDetails.TradingHours is null) {
+            return [];
+        }
+        
         return DateInterval.FromIbApiMessage(contractDetails.TimeZoneId, contractDetails.TradingHours);
     }
 }
